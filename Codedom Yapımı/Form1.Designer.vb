9bd0c6fb42df8ecaf9628c61af6e5439106b6453
Utku Halis <20300056@emu.edu.tr>
2021-04-22 13:11:00 +0300

    Initial commit

---
+++ Codedom Yapımı/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStrip2 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1.SuspendLayout()
+        Me.ToolStrip2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.BackColor = System.Drawing.Color.White
+        Me.ToolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton2})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(794, 25)
+        Me.ToolStrip1.TabIndex = 0
+        '
+        'ToolStrip2
+        '
+        Me.ToolStrip2.BackColor = System.Drawing.Color.White
+        Me.ToolStrip2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ToolStrip2.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.ToolStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1})
+        Me.ToolStrip2.Location = New System.Drawing.Point(0, 546)
+        Me.ToolStrip2.Name = "ToolStrip2"
+        Me.ToolStrip2.Size = New System.Drawing.Size(794, 25)
+        Me.ToolStrip2.TabIndex = 1
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(48, 22)
+        Me.ToolStripButton1.Text = "Durum"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.RichTextBox2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 387)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(794, 159)
+        Me.Panel1.TabIndex = 2
+        Me.Panel1.Visible = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Font = New System.Drawing.Font("Rockwell", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 25)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(794, 362)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = "Imports System.Windows.Forms" & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "Public Class VBClass" & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "Public Function Code()" & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "Messa" & _
+            "gebox.Show(""Merhaba, Dünya."")" & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "End Function" & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "End Class"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox2.Font = New System.Drawing.Font("Rockwell", 9.75!)
+        Me.RichTextBox2.Location = New System.Drawing.Point(0, 0)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(792, 157)
+        Me.RichTextBox2.TabIndex = 0
+        Me.RichTextBox2.Text = ""
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(47, 22)
+        Me.ToolStripButton2.Text = "Çalıştır"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(794, 571)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.ToolStrip2)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Codedom Yapımı"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ToolStrip2.ResumeLayout(False)
+        Me.ToolStrip2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents ToolStrip2 As System.Windows.Forms.ToolStrip
+    Friend WithEvents ToolStripButton1 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents ToolStripButton2 As System.Windows.Forms.ToolStripButton
+
+End Class
---
+++ Codedom Yapımı/Form1.vb
@@ -0,0 +1,119 @@
+﻿'Codedom Nedir?
+'Arkadaşlar Codedom her dilde vardır en basit visual basic6'dan en büyük java dillerine kadar vardır.
+'Codedom programımızı hangi programlama dilinde yaptıysak bize o dilde program yazmayı sağlar.
+'VB.Net için yaptığımızı düşünelim neredeyse 3Gb Visual Studio yerine kendi yaptığımız bir program ile başka bir bilgisayarda
+'VB.Net dilinde program yazabiliriz.
+Public Class Form1
+
+    Dim objCodeCompiler As System.CodeDom.Compiler.ICodeCompiler = New VBCodeProvider().CreateCompiler
+    Dim objCompilerParameters As New System.CodeDom.Compiler.CompilerParameters()
+
+#Region "Çalıştır"
+
+
+
+
+    Sub Execute()
+
+        objCompilerParameters.ReferencedAssemblies.Add("System.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Core.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Data.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Data.DataSetExtensions.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Windows.Forms.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Deployment.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Drawing.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Management.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Xml.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Xml.Linq.dll")
+        objCompilerParameters.ReferencedAssemblies.Add("System.Net.dll")
+
+        objCompilerParameters.GenerateInMemory = True
+
+        Dim strCode As String = RichTextBox1.Text
+
+        Dim objCompileResults As System.CodeDom.Compiler.CompilerResults = objCodeCompiler.CompileAssemblyFromSource(objCompilerParameters, strCode)
+
+        If objCompileResults.Errors.HasErrors Then
+            RichTextBox2.Text &= vbNewLine & "Hata: Satır>" & objCompileResults.Errors(0).Line.ToString & ", " & objCompileResults.Errors(0).ErrorText
+            Exit Sub
+        End If
+
+        Dim objAssembly As System.Reflection.Assembly = objCompileResults.CompiledAssembly
+
+        Dim objTheClass As Object = objAssembly.CreateInstance("VBClass")
+        If objTheClass Is Nothing Then
+            If RichTextBox1.Text = "" Then
+                RichTextBox2.Text &= vbNewLine & "Boş Sayfa"
+            End If
+            Exit Sub
+        End If
+
+        Try
+            RichTextBox2.Text &= "Kodlar Çözümleniyor..." & vbNewLine
+            Execute2()
+            RichTextBox2.Text &= vbNewLine & "Dosya Kayıt Ediliyor..." & vbNewLine
+            My.Computer.FileSystem.CreateDirectory(My.Computer.FileSystem.SpecialDirectories.Desktop & "\")
+            RichTextBox1.SaveFile(My.Computer.FileSystem.SpecialDirectories.Desktop & "\test.vb", RichTextBoxStreamType.RichText)
+
+
+            RichTextBox2.Text &= "Kodlar Tekrar Çözümleniyor..." & vbNewLine
+            RichTextBox1.LoadFile(My.Computer.FileSystem.SpecialDirectories.Desktop & "\test.vb")
+            Execute2()
+            RichTextBox2.Text &= vbNewLine & "Başlatıldı.."
+            objTheClass.GetType.InvokeMember("Code", System.Reflection.BindingFlags.InvokeMethod, Nothing, objTheClass, Nothing)
+        Catch ex As Exception
+            RichTextBox2.Text &= vbNewLine & "Hata:" & ex.Message
+
+        End Try
+    End Sub
+#End Region
+
+#Region "Hata Tara"
+    Sub Execute2()
+
+
+        Dim strCode As String = RichTextBox1.Text
+
+
+        Dim objCompileResults As System.CodeDom.Compiler.CompilerResults = objCodeCompiler.CompileAssemblyFromSource(objCompilerParameters, strCode)
+
+
+        If objCompileResults.Errors.HasErrors Then
+            RichTextBox2.Text &= vbNewLine & "Hata: Satır>" & objCompileResults.Errors(0).Line.ToString & ", " & objCompileResults.Errors(0).ErrorText
+            Exit Sub
+        End If
+
+        Dim objAssembly As System.Reflection.Assembly = objCompileResults.CompiledAssembly
+
+        Dim objTheClass As Object = objAssembly.CreateInstance("VBClass")
+        If objTheClass Is Nothing Then
+            If RichTextBox1.Text = "" Then
+                RichTextBox2.Text &= vbNewLine & "Boş Sayfa"
+            End If
+            Exit Sub
+        End If
+
+    End Sub
+#End Region
+
+    Private Sub ToolStripButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton1.Click
+        If Panel1.Visible = True Then
+            Panel1.Visible = False
+        Else
+            Panel1.Visible = True
+        End If
+    End Sub
+
+    Private Sub ToolStripButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton2.Click
+        If Panel1.Visible = False Then
+            Panel1.Visible = True
+        End If
+
+        RichTextBox2.Clear()
+        Execute()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
